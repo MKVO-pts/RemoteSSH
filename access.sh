573ce77d67974461afb02c34a3dc384be0f96c91
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2020-12-22 17:14:20 +0000

    Update access.sh

--- access.sh
+++ access.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-clear
-echo " _______________________________ "
-echo "| %     % %    %        %%%%%%  |"
-echo "| %%   %% %   %         %    %  |"
-echo "| % % % % %  %   %    % %    %  |"
-echo "| %  %  % %%%    %    % %    %  |"
-echo "| %     % %  %   %    % %    %  |"
-echo "| %     % %   %   %  %  %    %  |"
-echo "| %     % %    %   %%   %%%%%%  |"
-echo "\_______________________________/"
+
+#var whit colors 
+RED='\033[0;31m'
+MANGENTA='\e[95m'
+GREEN='\e[92m'
+CIAN='\e[96m'
+YELLOW='\e[93m'
+NC='\033[0m' # No Color
+clear #clear output
+
+echo -e ${RED} "Made By "
+echo -e " ${CIAN} _______________________________ "
+echo " /                               \ "
+echo -e " ${CIAN}|${GREEN} %     % %    %        %%%%%% ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} %%   %% %   %         %    % ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} % % % % %  %   %    % %    % ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} %  %  % %%%    %    % %    % ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} %     % %  %   %    % %    % ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} %     % %   %   %  %  %    % ${CIAN} |${NC}"
+echo -e " ${CIAN}|${GREEN} %     % %    %   %%   %%%%%% ${CIAN} |${NC}"
+echo -e " ${CIAN}\_______________________________/${NC}"
 echo ""
  
-echo Access Tool!
-echo This tool is used to remotly acess my personal computer and run commands.
-echo First configure the values in cfg.txt 
-sleep 5        #timeout de 7 segundos
+echo -e ${YELLOW} Access Tool!
+echo -e ${MANGENTA} This tool is used to remotly acess my personal computer and run commands.
+echo -e ${MANGENTA} DONT forget to configure first the values in cfg.txt!
+echo -e ${MANGENTA} To leave the server/machine unse the command "logout"
+
+sleep 5        #timeout de 5 segundos
 clear
-echo Creating ssh tuneling...
-bash cfg.sh      #faz a ligacao
+echo -e ${GREEN} Creating ssh tuneling...
+bash cfg.sh      #run other file
