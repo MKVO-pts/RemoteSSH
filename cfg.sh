f3fffabf3dcc67a92e01f6787da6a8fd083dd289
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2020-12-22 17:18:54 +0000

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-# Muda as variaveis conforme o servidor remoto
-username="sound"
-ip="192.168.1.205"
-password=cloud
+#update whit your server info
+username=""
+ip=""
+password=""
 
 
 #dont change this code
 warning="You havent config the cfg.sh file"
+YELLOW='\e[93m'
+GREEN='\e[92m'
+
+
+#verify if the username,ip,password are not empy
+echo -e ${GREEN} "Exporting cfg file..."
 if [ $username == "" ]; then
-	echo $warning
+	echo -e ${YELLOW} $warning
 	read -p "Enter your remote username : " username
   
 	elif [ $ip == "" ]; then
-		echo $warning
+		echo -e ${YELLOW} $warning
 		read -p "Enter your remote ip addr: " ip
 
 	elif [ $password == "" ]; then
-		echo $warning
+		echo -e ${YELLOW} $warning
 		read -p -s "Enter your remote password: " password
-fi
+		
+    else 
+	    echo -e ${GREEN} "Exported whit sucess!"
+fi	
 
 #dont change this command
+#this command start a connection betwen the server and de client
 ssh $username@$ip
-
