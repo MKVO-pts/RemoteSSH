df83974549c5343bd413c1f98e55202fb2a9912c
MkvO <76405447+MKVO-pts@users.noreply.github.com>
2020-12-22 18:51:24 +0000

    Update cfg.sh

--- cfg.sh
+++ cfg.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
 #update whit your server info
-username=""
-ip=""
-password=""
+username=""         #put your remote user
+ip=""               #put the remote hostname 
+password=""	    # put something, dont acctually have to be the passowrd
 
 
+
+
+
+
+##
 #dont change this code
 warning="You havent config the cfg.sh file"
 YELLOW='\e[93m'
 GREEN='\e[92m'
-
-
+NC='\033[0m'
 #verify if the username,ip,password are not empy
 echo -e ${GREEN} "Exporting cfg file..."
-if [ $username == "" ]; then
+if [ $username = "" ]; then
 	echo -e ${YELLOW} $warning
 	read -p "Enter your remote username : " username
   
-	elif [ $ip == "" ]; then
+	elif [ $ip = "" ]; then
 		echo -e ${YELLOW} $warning
 		read -p "Enter your remote ip addr: " ip
 
-	elif [ $password == "" ]; then
+	elif [ $password = "" ]; then
 		echo -e ${YELLOW} $warning
 		read -p -s "Enter your remote password: " password
 		
