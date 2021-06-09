909360416686be76528c2f0fc0273ac659e15805
MKVO-pts <tmricardopt@gmail.com>
2021-06-09 21:45:48 +0100

    Working update + less files

--- access.sh
+++ access.sh
@@ -9,43 +9,12 @@ YELLOW='\e[93m'
 NC='\033[0m' # No Color
 clear #clear output
 
-echo -e ${RED} "Made By "
-echo -e " ${CIAN} _______________________________ "
-echo " /                               \ "
-echo -e " ${CIAN}|${GREEN} %     % %    %        %%%%%% ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} %%   %% %   %         %    % ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} % % % % %  %   %    % %    % ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} %  %  % %%%    %    % %    % ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} %     % %  %   %    % %    % ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} %     % %   %   %  %  %    % ${CIAN} |${NC}"
-echo -e " ${CIAN}|${GREEN} %     % %    %   %%   %%%%%% ${CIAN} |${NC}"
-echo -e " ${CIAN}\_______________________________/${NC}"
-echo ""
- 
-echo -e ${YELLOW} Access Tool!
-echo -e ${MANGENTA} This tool is used to remotly acess my personal computer and run commands.
-echo -e ${MANGENTA} DONT forget to configure first the values in cfg.txt!
-echo -e ${MANGENTA} To leave the server/machine unse the command "logout"
-sleep 3        #timeout de 5 segundos
-clear
 
 
-#menu
-echo Choose one option:
-echo -e "${CIAN}____________________________________"
-echo -e "${CIAN}|  [1]	|${GREEN} Create a SSH connection ${CIAN} |${NC}"
-echo -e "${CIAN}|  [2]	|${GREEN} Add a new connection ${CIAN}    |${NC}"
-echo -e "${CIAN}|  [3]	|${GREEN} Remove one device ${CIAN}	   |${NC}"
-echo -e "${CIAN}|  [4]	|${GREEN} Exit ${CIAN}			   |${NC}"
-echo -e "${CIAN}|__________________________________|"
 
 
-echo Option: 
-read option
-clear
-
-
-#List devices stored in "options" file
+#OPTION 1 AND 3
+#List devices stored in "options" dir
 select_device() {
 	cd options
 	base=0
@@ -58,76 +27,125 @@ select_device() {
 
 	echo "Select one option:"
 	read device		#user input 	
-	
+
 
 	#select the option choosed
-	base_2=0
+	local base_2=0
 	for file in $commands; do 
-		if [[] $base_2 -eq $device]; then
-						
-			base_2=$(($base_2+1))
-			return $file
+		if [[ $base_2 == $device ]]; then
+			selected = $file
 			break
 		fi
+		base_2=$(($base_2+1))
 	done
 }
 
 
-
+#OPTION 1
 #connect to a stored device
 conect() {
-	echo grub-mklayout	
+	source $file.sh
+	echo Importing "$file" data ...
+	sleep 1
+	echo Connecting ...
+	sleep 1
+	ssh $username@$ip
 }
 
 
-
-
-
-
+#OPTION 2
 #add a new device
+new_device() {
+	cd options
+	echo Choose your file name:
+	read file_name
+	echo Enter the device Username:
+	read username
+	echo Enter the device IP:
+	read IP
+	echo Enter the device passowrd:
+	read password
+
+	echo Creating the file...
+	touch $file_name
+	echo -e Username=$username >> $file_name
+	echo -e IP=$IP >> $file_name
+	echo -e Password=$password >> $file_name
+
+}
 
 
+#menu
+menu() {
+
+	#normal menu	
+	echo -e ${RED} "Made By "
+	echo -e " ${CIAN} _______________________________ "
+	echo -e " /                               \ "
+	echo -e " ${CIAN}|${GREEN} %     % %    %        %%%%%% ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} %%   %% %   %         %    % ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} % % % % %  %   %    % %    % ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} %  %  % %%%    %    % %    % ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} %     % %  %   %    % %    % ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} %     % %   %   %  %  %    % ${CIAN} |${NC}"
+	echo -e " ${CIAN}|${GREEN} %     % %    %   %%   %%%%%% ${CIAN} |${NC}"
+	echo -e " ${CIAN}\_______________________________/${NC}"
+	echo -e " "
+ 
+	echo -e ${YELLOW} Access Tool!
+	echo -e ${MANGENTA} This tool is used to remotly acess my personal computer and run commands.
+	echo -e ${MANGENTA} DONT forget to configure first the values in cfg.txt!
+	echo -e ${MANGENTA} To leave the server/machine unse the command "logout"
+
+	#normal menu options
+	echo Choose one option:
+	echo -e "${CIAN}____________________________________"
+	echo -e "${CIAN}|  [1]	|${GREEN} Create a SSH connection ${CIAN} |${NC}"
+	echo -e "${CIAN}|  [2]	|${GREEN} Add a new connection ${CIAN}    |${NC}"
+	echo -e "${CIAN}|  [3]	|${GREEN} Remove one device ${CIAN}	   |${NC}"
+	echo -e "${CIAN}|  [4]	|${GREEN} Exit ${CIAN}			   |${NC}"
+	echo -e "${CIAN}|__________________________________|"
+	echo Option: && read option
+
+	#LOOP
+	#option 1, connect to a device
+	if [ $option -eq 1 ]; then
+		select_device
+		echo $selected
+		connect
+		sleep 3
+		menu
+
+	#option 2, create a new connection
+	elif [[ $option -eq 2 ]]; then
+		new_device
+		sleep 2
+		menu
+
+	#option 3, remove a stored device data
+	elif [ $option -eq 3 ]; then
+		select_device
+		echo File: $file
+		echo Are you sure you want to delet "$file"? [y/n]
+		read confirmation
+		if [ $confirmation -eq y ]; then
+			rm $file
+		fi
+		menu
+
+	#option 4,close the tool
+	elif [ $options -eq 4 ]; then
+		echo -e "${RED} Closing the app..."
+		sleep 3
+		exit
+
+	#to unknown input or commands
+	else
+		echo -e "${RED} Unknown command!"
+		sleep 2
+		menu
+	fi
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if [ $option -eq 1 ]; then
-	select_device
-	echo -e "${YELLOW}Select one device" && read selected
-	echo -e "${GREEN} Creating ssh tuneling... ${NC}"
-
-
-elif [[ $option=2 ]]; then
-	echo Not
-
-elif [[ $option==3 ]]; then
-	select_device
-	
-
-elif [ $options=4 ]; then
-	echo -e "${RED} Closing the app..."
-	sleep 3
-	exit
-elif [ $options=3 ]; then
-	echo ""
-
-else
-	echo "${RED} Unknown command"
-
-fi
-
-
+menu #call function menu
